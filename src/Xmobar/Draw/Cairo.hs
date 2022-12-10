09d966a58975e8322054c66941e776e5fcfa5e13
jao <jao@gnu.org>
2022-12-10 18:57:18 +0000

    off by one

--- src/Xmobar/Draw/Cairo.hs
+++ src/Xmobar/Draw/Cairo.hs
@@ -181,8 +181,8 @@ drawSegments dctx surf = do
 #endif
   (lend, as, bx) <- foldM (drawSegment dctx surf dw) (0, [], []) llyts
   let [rw, cw] = map sWidth [rlyts, clyts]
-      rstart = max (lend + 1) (dw - rw - 1)
-      cstart = max (lend + 1) ((dw - cw) / 2.0)
+      rstart = max lend (dw - rw - 1)
+      cstart = max lend ((dw - cw) / 2.0)
       cmax = rstart - 1
   (_, as', bx') <- foldM (drawSegment dctx surf cmax) (cstart, as, bx) clyts
   (_, as'', bx'') <- foldM (drawSegment dctx surf dw) (rstart, as', bx') rlyts
